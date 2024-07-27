6f220204d18964c0c8d9383f7cf75c45b2bc8d97
Lila Amelia <lilaamelia77@gmail.com>
2024-07-27 11:45:49 +0700

    api successfully created

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {
+        'name': 'Lila', 
+        'age': 25
+    }
+    return Response(person)
